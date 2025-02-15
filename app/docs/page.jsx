59c9eaa2a2903ba55e7ea4c84c7ddee12bb07b2b
Aditya Patil <adpatil@redhat.com>
2025-02-15 15:40:28 +0530

    added scrolling functionality to docs page

--- app/docs/page.jsx
+++ app/docs/page.jsx
@@ -1,20 +1,24 @@
 const descriptionSections = [
   {
+    id: "1",
     title: "What is Zen UI",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
+    id: "2",
     title: "Why use Zen UI",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
+    id: "3",
     title: "What we have used",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
+    id: "4",
     title: "How to install",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -22,6 +26,8 @@ const descriptionSections = [
 ];
 
 const Description = () => {
+  
+
   return (
     <div className="flex justify-between m-auto w-[1440px] mt-[100px]">
       <div className="sticky top-24 self-start overflow-y-auto w-[300px]">
@@ -30,17 +36,17 @@ const Description = () => {
           <div className="h-auto border-l-2 border-dark-blue opacity-65 ml-1 mr-5 mt-1 mb-3"></div>
           <div>
             {descriptionSections.sort().map((item) => {
-              return <p className="text-md mb-2 hover:scale-125 hover:cursor-pointer origin-left  duration-200 ease-in-out">{item.title}</p>;
+              return <p className="text-md mb-2 hover:scale-125 hover:cursor-pointer origin-left  duration-200 ease-in-out"><a href={`#${item.id}`}>{item.title}</a></p>;
             })}
           </div>
         </div>
       </div>
       <div className="w-full ">
         {descriptionSections.map((item) => (
-          <>
+          <div className="scroll-pt-5" id={item.id}>
             <p className="text-3xl font-semibold mb-3">{item.title}</p>
-            <p className="leading-6 mb-8">{item.description}</p>
-          </>
+            <p className="leading-6 mb-[500px]">{item.description}</p>
+          </div>
         ))}
       </div>
     </div>
