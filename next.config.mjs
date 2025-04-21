7c545fcfa2cf267b6943d89750fe29e57ad3053a
Aditya Patil <adpatil@redhat.com>
2025-04-21 23:21:56 +0530

    Added .yml file for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    assetPrefix: '/zenui',
+    basePath: '/zenui',
+};
 
 export default nextConfig;
